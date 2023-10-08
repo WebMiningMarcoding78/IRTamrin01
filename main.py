2855d6473b56d730d99ac4067abced01ad52f7d2
Mohammad Amin Rahimi(Laptop) <marcoding78@gmail.com>
2023-10-08 19:09:04 +0330

    fix: fix tokenizer problem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
 from hazm import *
+import os
 
 def extract_persian_text(file_path):
     with open(file_path, 'rb') as file:
@@ -26,36 +27,58 @@ def save_persian_text(file_path, persian_text):
         for text in persian_text:
             file.write(text.strip() + '\n')
 
+def loadFirstTime():
+    # it must runs just for the first
+    srt_files = [
+        'texts/Skyscraper.2018.720p.WEB-DL.H264.AC3-EVO.[UTF-8].srt',
+        'texts/Sleepless.2017.BDRip.x264-GECKOS.(www.esubtitle.com).srt',
+        'texts/Star.Wars.TheLast.Jedi.2017.HC.720p.HDTC.MkvCage.srt',
+        'texts/The.Hateful.Eight.2015.720p.BluRay.x264.SPARKS.srt',
+        'texts/We_Have_Always_Lived_in_the_Castle.srt'
+    ]
+
+    for file_path in srt_files:
+        persian_text = extract_persian_text(file_path)
+        save_persian_text(file_path, persian_text)
+
+def saveListTofiles(file_path, my_list):
+    with open(file_path, 'w') as file:
+        for item in my_list:
+            file.write(str(item) + "\n")
+            if len(item) > 8 : 
+                print(item)
+
+
 
 def main():
+    # loadFirstTime()
     # films = ["Skyscraper_persian.txt","Sleepless_persian.txt","Star_persian.txt","The_persian.txt","We_Have_Always_Lived_in_the_Castle_persian.txt"]
-    films = ["We_Have_Always_Lived_in_the_Castle_persian.txt"]
+    films = ["Skyscraper_persian.txt"]
     
-    ## it must runs just for the first
-    # srt_files = [
-    #     'texts/Skyscraper.2018.720p.WEB-DL.H264.AC3-EVO.[UTF-8].srt',
-    #     'texts/Sleepless.2017.BDRip.x264-GECKOS.(www.esubtitle.com).srt',
-    #     'texts/Star.Wars.TheLast.Jedi.2017.HC.720p.HDTC.MkvCage.srt',
-    #     'texts/The.Hateful.Eight.2015.720p.BluRay.x264.SPARKS.srt',
-    #     'texts/We_Have_Always_Lived_in_the_Castle.srt'
-    # ]
-
-    # for file_path in srt_files:
-    #     persian_text = extract_persian_text(file_path)
-    #     save_persian_text(file_path, persian_text)
-
+    
     for film in films :
         filmSentences = findallSentences(film)
-        normalizer = Normalizer()
-        normalize_text = normalizer.normalize(filmSentences)
+        normalize_text = normalizer(filmSentences)
+        tokenizedTokens = tokenize(normalize_text)
+        saveListTofiles("tokens/"+ film,tokenizedTokens)
+        # print(tokenizedTokens)
+        
 
-        word_tokenizer = WordTokenizer()
-        lexical_tokens = word_tokenizer.tokenize(normalize_text)
+def normalizer(filmSentences):
+    normalizer = Normalizer(
+                            remove_diacritics=True,
+                            remove_specials_chars=True,
+                            decrease_repeated_chars=True,
+                            unicodes_replacement=True,
+                            persian_numbers=True,
+                            )
+    normalize_text = normalizer.normalize(filmSentences)
+    return normalize_text
 
-        splitter = TokenSplitter()
-        splitted_tokens = [splitter.split_token_words(token) for token in lexical_tokens]
-        print(splitted_tokens)
-    
+def tokenize(normalize_text):
+    tokenizer = WordTokenizer()
+    tokenizedTokens = tokenizer.tokenize(normalize_text)
+    return tokenizedTokens
 
 def findallSentences(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
